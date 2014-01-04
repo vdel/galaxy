6ddf7d169199993a59bacabf1b42910fe38433ec
Vincent Delaitre <vincent.delaitre@gmail.com>
2014-01-04 23:09:00 +0100

    archi

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/local/vdelaitr/python/lib/python2.7/site-packages/
-export THEANO_FLAGS=device=gpu0,base_compiledir=/meleze/data2/vdelaitr/theano
+export THEANO_FLAGS=device=gpu0,base_compiledir=/meleze/data2/vdelaitr/theano,nvcc.flags='--gpu-architecture=sm_30'
 python main.py
