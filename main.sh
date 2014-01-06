9e7dd37b2e92f0f40a67ca24fa4dbb9ece39cf6b
Vincent Delaitre <vincent.delaitre@gmail.com>
2014-01-06 12:39:55 +0100

    added to path

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PATH=$PATH:/usr/share/cuda-5.5/bin:/usr/share/cuda-5.5/include
-export PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/share/cuda-5.5/lib64
-export LD_LIBRARY_PATH
+#PATH=$PATH:/usr/share/cuda-5.5/bin:/usr/share/cuda-5.5/include
+#export PATH
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/share/cuda-5.5/lib64
+#export LD_LIBRARY_PATH
 export PYTHONPATH=/local/vdelaitr/python/lib/python2.7/site-packages/
 #export THEANO_FLAGS=device=gpu0,base_compiledir=/meleze/data2/vdelaitr/theano,nvcc.flags='-arch=sm_20'
 export THEANO_FLAGS=device=gpu0,base_compiledir=/meleze/data2/vdelaitr/theano
